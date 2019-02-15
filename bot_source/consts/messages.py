baeba9d833776c66814034f4eb59e93900b0e163
baaraban <perepichka@ucu.edu.ua>
2019-02-15 21:05:52 +0200

    Improve status handling

--- bot_source/consts/messages.py
+++ bot_source/consts/messages.py
@@ -1,3 +1,31 @@
-class Message:
-    Unauthorized = "You don't have permission to complete this action"
-    Banned = "You are banned from bot functionality" 
+from abc import ABC, abstractmethod
+from telegram.parsemode import ParseMode
+
+
+class Message(ABC):
+    def __init(self):
+        super().__init__()
+
+    @abstractmethod
+    def render(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_parser(self):
+        pass
+
+
+class UnauthorizedMessage(Message):
+    def render(self, **kwargs):
+        return "You don't have permission to complete this action"
+
+    def get_parser(self):
+        return ParseMode.HTML
+
+
+class BannedMessage(Message):
+    def render(self, **kwargs):
+        return "You are banned from bot functionality"
+
+    def get_parser(self):
+        return ParseMode.HTML
--- bot_source/services/status_handler.py
+++ bot_source/services/status_handler.py
@@ -1,13 +1,18 @@
 from consts.statuses import Status
-from consts.messages import Message
+from consts.messages import *
 
 
 class StatusHandler:
     _handling_dict = {
-        Status.Unauthorized: Message.Unauthorized,
-        Status.Banned: Message.Banned
+        Status.Unauthorized: UnauthorizedMessage(),
+        Status.Banned: BannedMessage()
     }
 
     @staticmethod
-    def handle_status(bot, update, status):
-        bot.send_message(chat_id=update.message.chat_id, text=StatusHandler._handling_dict[status])
+    def handle_status(bot, update, status, **kwargs):
+        message = StatusHandler._handling_dict[status]
+        text = message.render(**kwargs)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=text,
+                         parser=message.get_parser()
+                         )
